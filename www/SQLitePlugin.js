litehelpers/cordova-sqlite-evplus-ext-legacy-build-free

commit dde754bed6bbccab42e6e689a45652ae00188fc0

Fix error code member & add sqliteCode [Android/iOS] (ref: litehelpers/Cordova-sqlite-storage#366)

www/SQLitePlugin.js
@@ -437,12 +437,15 @@ Contact for commercial license: info@litehelpers.net
     tx = this;
     handlerFor = function(index, didSucceed) {
       return function(response) {
-        var err;
+        var err, sqlError;
         try {
           if (didSucceed) {
             tx.handleStatementSuccess(batchExecutes[index].success, response);
           } else {
-            tx.handleStatementFailure(batchExecutes[index].error, newSQLError(response));
+            sqlError = newSQLError(response);
+            sqlError.code = response.result.code;
+            sqlError.sqliteCode = response.result.sqliteCode;
+            tx.handleStatementFailure(batchExecutes[index].error, sqlError);
           }
         } catch (_error) {
           err = _error;
@@ -491,7 +494,7 @@ Contact for commercial license: info@litehelpers.net
       i++;
     }
     mycb = function(result) {
-      var c, changes, errormessage, insert_id, j, k, q, r, ri, rl, row, rows, v;
+      var c, changes, code, errormessage, insert_id, j, k, q, r, ri, rl, row, rows, sqliteCode, v;
       i = 0;
       ri = 0;
       rl = result.length;
@@ -539,10 +542,14 @@ Contact for commercial license: info@litehelpers.net
             insertId: insert_id
           });
           ++ri;
-        } else if (r === 'errormessage') {
+        } else if (r === 'error') {
+          code = result[ri++];
+          sqliteCode = result[ri++];
           errormessage = result[ri++];
           q.error({
             result: {
+              code: code,
+              sqliteCode: sqliteCode,
               message: errormessage
             }
           });

spec/www/spec/legacy.js
@@ -1238,15 +1238,28 @@ var mytests = function() {
             }, function(tx, error) {
               ok(!!error, "valid error object");
 
-              // XXX ALSO BROKEN for iOS in this version:
-              //if (isWebSql || !(isAndroid || isWindows || isWP8))
-              //  ok(!!error['code'], "valid error.code exists");
+              // XXX NOT WORKING for Windows version of plugin:
+              if (!isWindows)
+                ok(!!error['code'], "valid error.code exists");
 
               ok(error.hasOwnProperty('message'), "error.message exists");
-              // XXX ALSO BROKEN for iOS in this version:
-              //if (isWebSql || !(isAndroid || isWindows || isWP8))
-              //  strictEqual(error.code, 5, "error.code === SQLException.SYNTAX_ERR (5)");
+
+              // XXX NOT WORKING for Web SQL or Windows version of plugin:
+              //if (!(isWebSql || isWindows))
+              // XXX NOT WORKING for Windows version of plugin:
+              if (!isWindows)
+                strictEqual(error.code, 5, "error.code === SQLException.SYNTAX_ERR (5)");
+
               //equal(error.message, "Request failed: insert into test_table (data) VALUES ,123", "error.message");
+
+              // XXX NOT WORKING for Web SQL or Windows version of plugin:
+              if (!(isWebSql || isWindows))
+                ok(!!error['sqliteCode'], "valid error.sqliteCode exists");
+
+              // XXX NOT WORKING for Web SQL or Windows version of plugin:
+              if (!(isWebSql || isWindows))
+                strictEqual(error.sqliteCode, 1, "error.sqliteCode === 1 (SQLITE_ERROR)");
+
               start();
 
               // We want this error to fail the entire transaction
@@ -1284,13 +1297,26 @@ var mytests = function() {
             }, function(tx, error) {
               ok(!!error, "valid error object");
 
-              // XXX ALSO BROKEN for iOS in this version:
-              //if (isWebSql || !(isAndroid || isWindows || isWP8))
-              //  ok(!!error['code'], "valid error.code exists");
+              // XXX NOT WORKING for Windows version of plugin:
+              if (!isWindows)
+                ok(!!error['code'], "valid error.code exists");
 
               ok(error.hasOwnProperty('message'), "error.message exists");
-              //strictEqual(error.code, 6, "error.code === SQLException.CONSTRAINT_ERR (6)");
+
+              // XXX NOT WORKING for Web SQL [iOS] or Windows version of plugin:
+              if (!(isWebSql || isWindows))
+                strictEqual(error.code, 6, "error.code === SQLException.CONSTRAINT_ERR (6)");
+
               //equal(error.message, "Request failed: insert into test_table (data) VALUES (?),123", "error.message");
+
+              // XXX NOT WORKING for Web SQL or Windows version of plugin:
+              if (!(isWebSql || isWindows))
+                ok(!!error['sqliteCode'], "valid error.sqliteCode exists");
+
+              // XXX NOT WORKING for Web SQL or Windows version of plugin:
+              if (!(isWebSql || isWindows))
+                strictEqual(error.sqliteCode, 19, "error.sqliteCode === 19 (SQLITE_CONSTRAINT)");
+
               start();
 
               // We want this error to fail the entire transaction

src/android/io/liteglue/SQLitePlugin.java
@@ -355,6 +355,7 @@ public class SQLitePlugin extends CordovaPlugin {
         long insertId = -1;
 
         String errorMessage = null;
+        int sqlite_error_code = -1;
 
         try {
             myStatement = mydbc.prepareStatement(query);
@@ -383,6 +384,11 @@ public class SQLitePlugin extends CordovaPlugin {
             if (rowsAffected > 0)
                 insertId = mydbc.getLastInsertRowid();
 
+        } catch (java.sql.SQLException ex) {
+            ex.printStackTrace();
+            sqlite_error_code= ex.getErrorCode();
+            errorMessage = ex.getMessage();
+            Log.e("executeSqlBatch", "SQLitePlugin.executeSql[Batch](): sqlite error code: " + sqlite_error_code + " message: " + errorMessage);
         } catch (Exception ex) {
             ex.printStackTrace();
             errorMessage = ex.getMessage();
@@ -434,7 +440,21 @@ public class SQLitePlugin extends CordovaPlugin {
 
             batchResultsList.put("endrows");
         } else if (errorMessage != null) {
-            batchResultsList.put("errormessage");
+            batchResultsList.put("error");
+            switch (sqlite_error_code) {
+            case SQLCode.ERROR:
+                batchResultsList.put(5); // SQLException.SYNTAX_ERR
+                break;
+            case 13: // SQLITE_FULL
+                batchResultsList.put(4); // SQLException.QUOTA_ERR
+                break;
+            case 19: // SQLITE_CONSTRAINT
+                batchResultsList.put(6); // SQLException.CONSTRAINT_ERR
+                break;
+            default:
+                batchResultsList.put(0); // SQLException.UNKNOWN_ERR
+            }
+            batchResultsList.put(sqlite_error_code);
             batchResultsList.put(errorMessage);
         } else if (rowsAffected > 0) {
             batchResultsList.put("ch2");

src/ios/SQLitePlugin.m
@@ -361,9 +361,9 @@
     if (error) {
         /* add error with result.message: */
 
-        // XXX FUTURE TBD: include full error object instead (??)
-        [results addObject:@"errormessage"];
-        //[results addObject:error];
+        [results addObject:@"error"];
+        [results addObject:[error objectForKey:@"code"]];
+        [results addObject:[error objectForKey:@"sqliteCode"]];
         [results addObject:[error objectForKey:@"message"]];
 
         return;
@@ -432,23 +432,19 @@
 +(NSDictionary *)captureSQLiteErrorFromDb:(struct sqlite3 *)db
 {
     int code = sqlite3_errcode(db);
-#if 0 // XXX TBD NOT USED IN THIS VERSION:
     int webSQLCode = [SQLitePlugin mapSQLiteErrorCode:code];
-#endif
-#if INCLUDE_SQLITE_ERROR_INFO
+#if 0 // XXX NOT SUPPORTED IN THIS VERSION:
     int extendedCode = sqlite3_extended_errcode(db);
 #endif
     const char *message = sqlite3_errmsg(db);
 
     NSMutableDictionary *error = [NSMutableDictionary dictionaryWithCapacity:4];
 
-#if 0 // XXX TBD NOT USED IN THIS VERSION:
     [error setObject:[NSNumber numberWithInt:webSQLCode] forKey:@"code"];
-#endif
     [error setObject:[NSString stringWithUTF8String:message] forKey:@"message"];
 
-#if INCLUDE_SQLITE_ERROR_INFO
     [error setObject:[NSNumber numberWithInt:code] forKey:@"sqliteCode"];
+#if 0 // XXX NOT SUPPORTED IN THIS VERSION:
     [error setObject:[NSNumber numberWithInt:extendedCode] forKey:@"sqliteExtendedCode"];
     [error setObject:[NSString stringWithUTF8String:message] forKey:@"sqliteMessage"];
 #endif
@@ -456,7 +452,6 @@
     return error;
 }
 
-#if 0 // XXX TBD NOT USED IN THIS VERSION:
 +(int)mapSQLiteErrorCode:(int)code
 {
     // map the sqlite error code to
@@ -472,6 +467,5 @@
             return UNKNOWN_ERR;
     }
 }
-#endif
 
 @end /* vim: set expandtab : */

commit 1b217a2c575e7be7845c49b17a0ea7307e67a84d

Fix bug with error reporting on Windows (introduced by dde754b) in this version branch
(www/SQLitePlugin.js regenerated using CoffeeScript 1.10.0)

www/SQLitePlugin.js
@@ -1,3 +1,4 @@
+
 /*
 License for this version: GPL v3 (http://www.gnu.org/licenses/gpl.txt) or commercial license.
 Contact for commercial license: info@litehelpers.net
@@ -316,14 +317,14 @@ Contact for commercial license: info@litehelpers.net
   };
 
   SQLitePluginTransaction.prototype.start = function() {
-    var err;
+    var err, error1;
     try {
       this.fn(this);
       if (this.executes.length > 0) {
         this.run();
       }
-    } catch (_error) {
-      err = _error;
+    } catch (error1) {
+      err = error1;
       txLocks[this.db.dbname].inProgress = false;
       this.db.startNextTransaction();
       if (this.error) {
@@ -437,18 +438,20 @@ Contact for commercial license: info@litehelpers.net
     tx = this;
     handlerFor = function(index, didSucceed) {
       return function(response) {
-        var err, sqlError;
+        var err, error1, sqlError;
         try {
           if (didSucceed) {
             tx.handleStatementSuccess(batchExecutes[index].success, response);
           } else {
             sqlError = newSQLError(response);
-            sqlError.code = response.result.code;
-            sqlError.sqliteCode = response.result.sqliteCode;
+            if (!!response.result) {
+              sqlError.code = response.result.code;
+              sqlError.sqliteCode = response.result.sqliteCode;
+            }
             tx.handleStatementFailure(batchExecutes[index].error, sqlError);
           }
-        } catch (_error) {
-          err = _error;
+        } catch (error1) {
+          err = error1;
           if (!txFailure) {
             txFailure = newSQLError(err);
           }